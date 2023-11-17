122eb442393f71f8ee0050fe7276a4f45d8bc1f3
“austinmula” <maustin64.ma@gmail.com>
2023-11-17 15:24:05 +0300

    hide topbar for main tab screens

--- App.js
+++ App.js
@@ -1,37 +1,55 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack'; 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {NavigationContainer} from '@react-navigation/native'
 
-import HomeScreen from './src/screens/home/home'; 
-import SettingScreen from './src/screens/home/settings';
-import AnalyticsScreen from './src/screens/home/analytics';
-import NotesScreen from './src/screens/home/notes';
-import TabBar from './src/components/common/TabBar';
+import HomeScreen from './src/screens/home/home'
+import SettingScreen from './src/screens/home/settings'
+import AnalyticsScreen from './src/screens/home/analytics'
+import NotesScreen from './src/screens/home/notes'
+import TabBar from './src/components/common/TabBar'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
-function HomeTabs() {
+function HomeTabs () {
   return (
-    <Tab.Navigator initialRouteName={"home"} tabBar={props => <TabBar {...props} />} >
-      <Tab.Screen name="home" component={HomeScreen} />
-      <Tab.Screen name="analytics" component={AnalyticsScreen} />
-      <Tab.Screen name="notes" component={NotesScreen} />
-      <Tab.Screen name="settings" component={SettingScreen} />
+    <Tab.Navigator initialRouteName={'home'}>
+      <Tab.Screen
+        options={{headerShown: false}}
+        name='home'
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{headerShown: false}}
+        name='analytics'
+        component={AnalyticsScreen}
+      />
+      <Tab.Screen
+        options={{headerShown: false}}
+        name='notes'
+        component={NotesScreen}
+      />
+      <Tab.Screen
+        options={{headerShown: false}}
+        name='settings'
+        component={SettingScreen}
+      />
     </Tab.Navigator>
-  );
+  )
 }
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
-function App(){
-  return(
+function App () {
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={"HomeTabs"} screenOptions={{headerShown:false}}>
-        <Stack.Screen name="HomeTabs" component={HomeTabs}/>
+      <Stack.Navigator
+        initialRouteName={'HomeTabs'}
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name='HomeTabs' component={HomeTabs} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-export default App; 
+export default App
