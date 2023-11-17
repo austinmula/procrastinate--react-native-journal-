3e3e1c3ab54a73eba5547bc62752bdcbed8e0374
“austinmula” <maustin64.ma@gmail.com>
2023-11-17 15:18:17 +0300

    placholder screens for navigation

---
+++ src/screens/home/notes.js
@@ -0,0 +1,11 @@
+const {View, Text} = require('react-native')
+
+const NotesScreen = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Text>HOME</Text>
+    </View>
+  )
+}
+
+export default NotesScreen;
