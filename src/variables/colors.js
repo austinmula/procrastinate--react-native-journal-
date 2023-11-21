e63a211d5ffc7c685cd8429844c56fc3be1a3928
“austinmula” <maustin64.ma@gmail.com>
2023-11-21 12:19:01 +0300

    adding color variables

---
+++ src/variables/colors.js
@@ -0,0 +1,13 @@
+const variables = {
+  colors: {
+    white: '#fff',
+    darkerbg: '#27233a',
+    darkbg: '#505168',
+    lighterBg: '#eaefd3',
+    lightBg: '#b3c0a4',
+    tan: '#dcc48e',
+    black: '#000',
+  },
+}
+
+export default variables
