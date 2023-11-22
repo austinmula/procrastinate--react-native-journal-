3ff64b9a7855704aba2b3a4acc0f97454a2b4007
“austinmula” <maustin64.ma@gmail.com>
2023-11-22 15:11:54 +0300

    creating basic sections for the hometab

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, StyleSheet, StatusBar, View} from 'react-native';
+import {Text, StyleSheet, StatusBar, View, Dimensions} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import variables from '../../variables/colors';
 
@@ -7,11 +7,14 @@ interface HomeScreenProps {
   navigation: any;
 }
 
+const {height, width} = Dimensions.get('window');
+
 const HomeScreen = ({navigation}: HomeScreenProps) => {
   return (
     <View style={styles.mainContainer}>
+      <StatusBar />
+      <View style={styles.headingContainer}></View>
       <SafeAreaView style={styles.container}>
-        <StatusBar />
         {/* <Text style={{fontSize: 30, fontWeight: '700'}}>Home</Text> */}
       </SafeAreaView>
     </View>
@@ -23,10 +26,16 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    // padding: 10,
   },
   mainContainer: {
     flex: 1,
     backgroundColor: variables.colors.lighterBg,
+  },
+  headingContainer: {
+    height: height * 0.2,
+    backgroundColor: variables.colors.lightBg,
+    borderBottomEndRadius: width * 0.1,
+    borderBottomLeftRadius: width * 0.1,
   },
 });
