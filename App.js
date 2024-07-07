72f673ec12762fe7245501364476453dbfc3bd7b
austinmula <maustin64.ma@gmail.com>
2024-07-07 15:46:42 +0300

    separating app navigator components from main app file

--- App.js
+++ App.js
@@ -1,117 +1,13 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
 
-import HomeScreen from './src/screens/home/home';
-import TasksScreen from './src/screens/home/tasks';
-import CalendarScreen from './src/screens/home/calendar';
-import GoalsScreen from './src/screens/home/targets';
-import TabBar from './src/components/common/TabBar';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import variables from './src/utils/variables/colors';
-import CreateJournalEntry from './src/screens/home/homestack/createJournalEntry';
-import SignInScreen from './src/screens/signin';
-import SignUpScreen from './src/screens/signup';
-
-const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
-
-function HomeTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName={'home'}
-      tabBar={props => <TabBar {...props} />}>
-      <Tab.Screen
-        options={{headerShown: false}}
-        name="home"
-        component={HomeStack}
-      />
-      <Tab.Screen
-        options={{headerShown: false}}
-        name="time"
-        component={GoalsScreen}
-      />
-      <Tab.Screen
-        options={{headerShown: false}}
-        name="list"
-        component={TasksScreen}
-      />
-      <Tab.Screen
-        options={{headerShown: false}}
-        name="calendar"
-        component={CalendarScreen}
-      />
-    </Tab.Navigator>
-  );
-}
-
-const Stack = createStackNavigator();
-
-const HomeStack = () => (
-  <Stack.Navigator initialRouteName="home">
-    <Stack.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{headerShown: false}}
-    />
-    <Stack.Screen
-      name="createEntry"
-      options={{headerShown: false}}
-      component={CreateJournalEntry}
-    />
-  </Stack.Navigator>
-);
-
-const AuthStack = () => {
-  return (
-    <Stack.Navigator initialRouteName="signin">
-      <Stack.Screen
-        name="signin"
-        component={SignInScreen}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="signup"
-        options={{headerShown: false}}
-        component={SignUpScreen}
-      />
-    </Stack.Navigator>
-  );
-};
-
-const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        drawerStyle: {
-          backgroundColor: variables.colors.lighterBg,
-        },
-        headerTintColor: variables.colors.darkbg,
-        // drawerType: 'front',
-        // drawerPosition: 'right',
-      }}>
-      <Drawer.Screen
-        options={{
-          // headerTransparent: true,
-          headerTitle: '',
-          headerStyle: {
-            backgroundColor: variables.colors.lightBg,
-          },
-        }}
-        name="Home"
-        component={HomeTabs}
-      />
-    </Drawer.Navigator>
-  );
-};
+import {AuthProvider} from './src/context/AuthContext';
+import AppNavigation from './AppNavigation';
 
 function App() {
   return (
-    <NavigationContainer>
-      {/* <DrawerNavigator /> */}
-      <AuthStack />
-    </NavigationContainer>
+    <AuthProvider>
+      <AppNavigation />
+    </AuthProvider>
   );
 }
 
---
+++ AppNavigation.js
@@ -0,0 +1,125 @@
+import React, {useContext} from 'react';
+import {AuthContext} from './src/context/AuthContext';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+
+import HomeScreen from './src/screens/home/home';
+import TasksScreen from './src/screens/home/tasks';
+import CalendarScreen from './src/screens/home/calendar';
+import GoalsScreen from './src/screens/home/targets';
+import TabBar from './src/components/common/TabBar';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import variables from './src/utils/variables/colors';
+import CreateJournalEntry from './src/screens/home/homestack/createJournalEntry';
+import SignInScreen from './src/screens/signin';
+import SignUpScreen from './src/screens/signup';
+import {View} from 'react-native';
+import {ActivityIndicator} from 'react-native-paper';
+
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName={'home'}
+      tabBar={props => <TabBar {...props} />}>
+      <Tab.Screen
+        options={{headerShown: false}}
+        name="home"
+        component={HomeStack}
+      />
+      <Tab.Screen
+        options={{headerShown: false}}
+        name="time"
+        component={GoalsScreen}
+      />
+      <Tab.Screen
+        options={{headerShown: false}}
+        name="list"
+        component={TasksScreen}
+      />
+      <Tab.Screen
+        options={{headerShown: false}}
+        name="calendar"
+        component={CalendarScreen}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const HomeStack = () => (
+  <Stack.Navigator initialRouteName="home">
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{headerShown: false}}
+    />
+    <Stack.Screen
+      name="createEntry"
+      options={{headerShown: false}}
+      component={CreateJournalEntry}
+    />
+  </Stack.Navigator>
+);
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="signin">
+      <Stack.Screen
+        name="signin"
+        component={SignInScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="signup"
+        options={{headerShown: false}}
+        component={SignUpScreen}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: variables.colors.lighterBg,
+        },
+        headerTintColor: variables.colors.darkbg,
+        // drawerType: 'front',
+        // drawerPosition: 'right',
+      }}>
+      <Drawer.Screen
+        options={{
+          // headerTransparent: true,
+          headerTitle: '',
+          headerStyle: {
+            backgroundColor: variables.colors.lightBg,
+          },
+        }}
+        name="Home"
+        component={HomeTabs}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default function AppNavigation() {
+  const {userToken, isLoading} = useContext(AuthContext);
+  if (isLoading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+  return (
+    <NavigationContainer>
+      {userToken === null ? <AuthStack /> : <DrawerNavigator />}
+    </NavigationContainer>
+  );
+}
